109128a20277c2525ae2f99e1724cc8d290445e3
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-03 06:40:48 +0000

    feat: allow running specific jobs from a pipeline

--- src/dagger/pipeline.ts
+++ src/dagger/pipeline.ts
@@ -1,10 +1,28 @@
 import Client, { connect } from "@dagger.io/dagger";
-import { fmt, lint, test } from "./jobs.ts";
+import * as jobs from "./jobs.ts";
 
-export default function pipeline(src = ".") {
+const { fmt, lint, test } = jobs;
+
+export default function pipeline(src = ".", args: string[] = []) {
   connect(async (client: Client) => {
+    if (args.length > 0) {
+      await runSpecificJobs(client, args);
+      return;
+    }
+
     await fmt(client, src);
     await lint(client, src);
     await test(client, src);
   });
+}
+
+async function runSpecificJobs(client: Client, args: string[]) {
+  for (const name of args) {
+    // deno-lint-ignore no-explicit-any
+    const job = (jobs as any)[name];
+    if (!job) {
+      throw new Error(`Job ${name} not found`);
+    }
+    await job(client);
+  }
 }
