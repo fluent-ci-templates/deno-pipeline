98c75b07cd994be26bdf000a4542b135582ab234
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-13 18:11:40 +0000

    feat: add codecov uploader job

--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -1,4 +1,5 @@
 import Client from "@dagger.io/dagger";
+import { upload } from "https://deno.land/x/codecov_pipeline@v0.1.0/src/dagger/jobs.ts";
 import { withDevbox } from "https://deno.land/x/nix_installer_pipeline@v0.3.6/src/dagger/steps.ts";
 import { existsSync } from "fs";
 
@@ -7,6 +8,7 @@ export enum Job {
   lint = "lint",
   test = "test",
   deploy = "deploy",
+  codecov = "codecov",
 }
 
 const baseCtr = (client: Client, pipeline: string) => {
@@ -176,11 +178,14 @@ export type JobExec = (
       }
     ) => Promise<void>);
 
+export const codecov = upload;
+
 export const runnableJobs: Record<Job, JobExec> = {
   [Job.fmt]: fmt,
   [Job.lint]: lint,
   [Job.test]: test,
   [Job.deploy]: deploy,
+  [Job.codecov]: upload,
 };
 
 export const jobDescriptions: Record<Job, string> = {
@@ -188,4 +193,5 @@ export const jobDescriptions: Record<Job, string> = {
   [Job.lint]: "Lint your code",
   [Job.test]: "Run your tests",
   [Job.deploy]: "Deploy your code to Deno Deploy",
+  [Job.codecov]: "Upload your code coverage to Codecov",
 };
