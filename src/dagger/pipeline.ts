1ded8edae973284f4845367b9f8560665c642625
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-01 07:38:47 +0300

    update pipeline.ts

--- src/dagger/pipeline.ts
+++ src/dagger/pipeline.ts
@@ -8,24 +8,22 @@ export default async function pipeline(src = ".", args: string[] = []) {
     await uploadContext(src, exclude);
   }
 
-  connect(async (client: Client) => {
-    if (args.length > 0) {
-      await runSpecificJobs(client, args as jobs.Job[]);
-      return;
-    }
+  if (args.length > 0) {
+    await runSpecificJobs(args as jobs.Job[]);
+    return;
+  }
 
-    await fmt(client, src);
-    await lint(client, src);
-    await test(client, src);
-  });
+  await fmt(src);
+  await lint(src);
+  await test(src);
 }
 
-async function runSpecificJobs(client: Client, args: jobs.Job[]) {
+async function runSpecificJobs(args: jobs.Job[]) {
   for (const name of args) {
     const job = runnableJobs[name];
     if (!job) {
       throw new Error(`Job ${name} not found`);
     }
-    await job(client);
+    await job();
   }
 }
